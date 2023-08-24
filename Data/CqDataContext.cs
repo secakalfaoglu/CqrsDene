556c56f74bdd3c1155a3c5384ff62e2090d39fdc
Ozkan Ozesme <ozkan.ozesme@tuseb.gov.tr>
2023-08-24 13:48:17 +0300

    first commit

---
+++ Data/CqDataContext.cs
@@ -0,0 +1,13 @@
+﻿using CqrsDene.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CqrsDene.Data;
+
+public class CqDataContext : DbContext
+{
+    public CqDataContext(DbContextOptions<CqDataContext> dbContextOptions) : base(dbContextOptions)
+    {
+    }
+
+    DbSet<Person> People { get; set; }
+}
---
+++ Models/Domain/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace CqrsDene.Models.Domain;
+
+public class Person
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+}
